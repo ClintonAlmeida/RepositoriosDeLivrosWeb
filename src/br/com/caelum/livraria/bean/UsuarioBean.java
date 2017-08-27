57840be31dfd6597668e61e89c46d250cea2df52
clinton.almeida <clinton.almeida@spnb02ti.GiraffasSP.com>
2017-08-26 22:19:38 -0300

    Foi adicionado a função que verifica se o na hora de cadastrar um usuario o e-mail já existe, se o email existir a o usuario não é cadastrado.

--- src/br/com/caelum/livraria/bean/UsuarioBean.java
+++ src/br/com/caelum/livraria/bean/UsuarioBean.java
@@ -3,10 +3,13 @@ package br.com.caelum.livraria.bean;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.dao.UsuarioDao;
 import br.com.caelum.livraria.modelo.Usuario;
 
 @ManagedBean
@@ -43,21 +46,31 @@ public class UsuarioBean {
 	}
 
 	public void gravar() {
-		System.out.println("Gravando autor " + this.usuario.getEmail());
-
-		// Verifica se autor existe, se ele não existir ele é salvo no banco
-		// caso contrario ele é atualizado
-		// está função está sendo usado no metodo update
-		if (this.usuario.getId() == null) {
-			new DAO<Usuario>(Usuario.class).adiciona(this.usuario);
-			this.enviar.sendMail("repositoriodelivrosdigitais@gmail.com", this.getUsuario().getEmail(),
-					"Criação de Conta",
-					"Ola, " + "\n" + "Usuario criado com sucesso, segue abaixo os dados da conta " + "\n" + "USUARIO: "
-							+ this.usuario.getEmail() + "\n" + "SENHA: " + this.usuario.getSenha());
-		} else {
-			new DAO<Usuario>(Usuario.class).atualiza(this.usuario);
+		
+		
+		FacesContext context = FacesContext.getCurrentInstance();
+		boolean existe = new UsuarioDao().existeEmail(this.usuario);//se o e-mail existir no sistema o valor é true
+		
+		if(existe) {
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
+			context.getExternalContext().getFlash().setKeepMessages(true);
+			context.addMessage(null, new FacesMessage("Já existe um usuario com este e-mail, use outro !"));
+			// Envia uma Mensagem para a tela	
 		}
-
+		
+		//Se o id for nulo e o email não está no sistema ele grava o usuario
+		if (this.usuario.getId() == null && existe == false) {
+			new DAO<Usuario>(Usuario.class).adiciona(this.usuario);
+			System.out.println(this.usuario + "Foi cadastrado");
+			//this.enviar.sendMail("repositoriodelivrosdigitais@gmail.com", this.getUsuario().getEmail(),
+			//		"Criação de Conta",
+			//		"Ola, " + "\n" + "Usuario criado com sucesso, segue abaixo os dados da conta " + "\n" + "USUARIO: "
+			//				+ this.usuario.getEmail() + "\n" + "SENHA: " + this.usuario.getSenha());
+		}else if(existe == false) {
+			new DAO<Usuario>(Usuario.class).atualiza(this.usuario);
+		}else {
+			
+		}
 		this.usuario = new Usuario();
 	}
 }
--- src/br/com/caelum/livraria/dao/UsuarioDao.java
+++ src/br/com/caelum/livraria/dao/UsuarioDao.java
@@ -1,5 +1,7 @@
 package br.com.caelum.livraria.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
@@ -9,27 +11,53 @@ import br.com.caelum.livraria.modelo.Usuario;
 public class UsuarioDao {
 
 	public boolean existe(Usuario usuario) {
-		
+
 		EntityManager em = new JPAUtil().getEntityManager();
-		
-		//Verifica se o email e senha do usuario existe no sistema
-		TypedQuery<Usuario> query = em.createQuery(
-				  " select u from Usuario u "
-				+ " where (u.email = :pEmail and u.senha = :pSenha) and "
-				+ "(u.ativo = :pAtivo)", Usuario.class);
-		
+
+		// Verifica se o email e senha do usuario existe no sistema
+		TypedQuery<Usuario> query = em.createQuery(" select u from Usuario u "
+				+ " where (u.email = :pEmail and u.senha = :pSenha) and " + "(u.ativo = :pAtivo)", Usuario.class);
+
 		query.setParameter("pEmail", usuario.getEmail());
 		query.setParameter("pSenha", usuario.getSenha());
 		query.setParameter("pAtivo", usuario.isAtivo());
 		try {
-			Usuario resultado =  query.getSingleResult();
+			Usuario resultado = query.getSingleResult();
 		} catch (NoResultException ex) {
 			return false;
 		}
-		
+
 		em.close();
-		
+
 		return true;
+	}
+
+	public boolean existeEmail(Usuario usuario) {
+
+		EntityManager em = new JPAUtil().getEntityManager();
+
+		// Verifica se o email e senha do usuario existe no sistema
+		TypedQuery<Usuario> query = em.createQuery(" Select u FROM Usuario u WHERE u.ativo = true and u.ativo = :pAtivo", Usuario.class);
+
+		query.setParameter("pAtivo", usuario.isAtivo());
+		
+		List<Usuario> usuarios = query.getResultList();
+		
+		
+		//Compara se existe o e-mail cadastrado na base de dados do sistema
+		for (Usuario usuario2 : usuarios) {
+			
+			if(usuario2.getEmail().equals(usuario.getEmail())) {
+				
+				System.out.println(usuario2.getEmail());
+				
+				return true;
+			}
+		}
+
+		em.close();
+
+		return false;
 	}
 
 }
