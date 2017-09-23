8504ee69ecb8c3c7783000003fcf4c07355983c2
Mateus <msilva.spa@hotmail.com>
2017-09-23 18:27:00 -0300

    mais uma tentativa

--- src/br/com/caelum/livraria/bean/FileUploadView.java
+++ src/br/com/caelum/livraria/bean/FileUploadView.java
@@ -4,25 +4,26 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 
+import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 
 @ManagedBean
 public class FileUploadView {
 
-    private UploadedFile file;
+	private UploadedFile file;
 
-    public UploadedFile getFile() {
-        return file;
-    }
+	public UploadedFile getFile() {
+		return file;
+	}
 
-    public void setFile(UploadedFile file) {
-        this.file = file;
-    }
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
 
-    public void upload() {
-        if(file != null) {
-            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
-            FacesContext.getCurrentInstance().addMessage(null, message);
-        }
-    }
+	public void upload() {
+		if (file != null) {
+			FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+			FacesContext.getCurrentInstance().addMessage(null, message);
+		}
+	}
 }
